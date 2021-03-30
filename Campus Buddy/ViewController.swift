447cd7f07dc4d5ec1753183937bc60e14a2dbc7a
Hamza Saleem <hsaleem@csumb.edu>
2021-03-29 22:19:37 -0700

    Initial Commit

---
+++ Campus Buddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Campus Buddy
+//
+//  Created by user190273 on 3/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
